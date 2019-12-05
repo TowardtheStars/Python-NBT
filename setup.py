43faaedeb919f2101e13e3ebcf45cf0dc63733b0
TowardtheStars <hty19980402@qq.com>
2019-12-05 09:08:07 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,27 +1,26 @@
 from setuptools import setup
 from python_nbt import VERSION
 
+with open("README.md", "r", encoding='utf-8') as file:
+  long_description = file.read()
+
 setup(
   name             = 'Python-NBT',
   version          = ".".join(str(x) for x in VERSION),
-  description      = 'NBT Reader/Writer',
+  description      = 'A python library for reading and writing NBT files',
   author           = 'TowardtheStars',
   author_email     = 'hty16@mail.ustc.edu.cn',
   url              = 'https://github.com/TowardtheStars/Python-NBT',
-  license          = open("LICENSE").read(),
-  long_description = open("README.txt").read(),
+  license          = "GPLv3",
+  long_description = long_description,
+  long_description_content_type="text/markdown",
   packages         = ['python_nbt'],
   classifiers      = [
         "Development Status :: 4 - Beta",
         "Intended Audience :: Developers",
         "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
         "Operating System :: OS Independent",
-        "Programming Language :: Python",
-        "Programming Language :: Python :: 3.3",
-        "Programming Language :: Python :: 3.4",
-        "Programming Language :: Python :: 3.5",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3",
         "Topic :: Games/Entertainment",
         "Topic :: Software Development :: Libraries :: Python Modules"
   ]
